6771dc9dbc8ee50c574d02b37f79dbddfad8e89a
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 20:22:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,8 @@
-var playlist = {};
+var playlist = {
+  artistName: "Senses Fail",
+  song: "Still Searching"
+};
 
-function updatePlaylist(playlist, artistName, song) {
-   Object.assign({}, plalist)
+function updatePlaylist(obj, artistName, song) {
+   Object.assign({}, obj, {artistName, song})
 }
